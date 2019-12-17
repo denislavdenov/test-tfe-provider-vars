2b60ed0ec899a0a8b6300456ada6d204b19dea70
Denislav Denov <denislavdenov@gmail.com>
2019-12-17 09:02:34 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "tfe" {}
+
+# Terraform workspace
+resource "tfe_workspace" "test" {
+  name = "test-tfe-provider"
+  organization = ddenov
+}
+
+resource "tfe_variable" "pcb_evars" {
+  for_each = var.envvarslist
+  key = each.key
+  value = each.value
+  category = "env"
+  workspace_id = tfe_workspace.test.id
+}
